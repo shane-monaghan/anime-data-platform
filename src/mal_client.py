0e5304027385343342e9098f72b478f895598081
shane-monaghan <135287482+shane-monaghan@users.noreply.github.com>
2026-07-29 02:49:05 +0000

    chore: Add docstrings and type hints

--- src/mal_client.py
+++ src/mal_client.py
@@ -1,7 +1,18 @@
 import requests
 
-class MAL_Client:
-    def __init__(self, client_id, base_url):
+class MALClient:
+    """
+    Represents a client for interacting with the My Anime List API.
+    """
+    def __init__(
+            self,
+            client_id: str,
+            base_url: str
+        ):
+        """
+        Initializes a MALClient instance.
+        Initializes authentication headers for requests to the MAL API.. 
+        """
         self.client_id = client_id
         self.base_url = base_url
 
@@ -9,7 +20,17 @@ class MAL_Client:
             "X-MAL-CLIENT-ID": self.client_id
         }
 
-    def get_seasonal_anime(self, year, season):
+    def get_seasonal_anime(
+            self,
+            year: int,
+            season: str
+        ) -> requests.Response:
+        """
+        Gets and returns a response from MAL's seasonal anime endpoint for a given year and season.
+
+        year: an integer e.g., 2009, 2021
+        season: "winter", "spring", "summer", "fall"
+        """
         endpoint = self.base_url + f"/season/{year}/{season}"
         response = requests.get(endpoint, headers=self.headers)
         return response
